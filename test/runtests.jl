be61b4e9d62ae43cf4b58312b2d8905334f29740
Dilum Aluthge <dilum@aluthge.com>
2026-01-27 19:37:27 -0500

    Remove a comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using Test
 
 @testset "Backporter Tests" begin
-    # Include and test individual modules
     include("cherry_pick.jl")
     include("git_operations.jl")
     include("pr_detection.jl")
